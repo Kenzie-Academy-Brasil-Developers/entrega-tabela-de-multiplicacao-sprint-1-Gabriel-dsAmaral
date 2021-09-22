d3caeeb4e8fd0c96b66fcfb6e4d7403d8d5b8e28
Amaral <foioamaral16@gmail.com>
2021-09-22 20:07:51 -0300

    Entrega corrigida

---
+++ script.js
@@ -0,0 +1,14 @@
+let table = function(x,y){
+    let result = []
+    for(let i = 0; i <= x; i++){
+        result.push([])
+        for (let j = 0; j <= y; j++){
+            result[i].push(i*j)
+        }
+    }return console.table(result) 
+}
+
+table(10,10)
+
+console.log('digite table(valorX da table,valorY da table) no console para ver a tabela com o tamanho desejado aparecer ;)')
+console.log('ex: table(16,16)')
