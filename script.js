34a9c315875fef65730c0c24b0b5e17772d15257
Amaral <foioamaral16@gmail.com>
2021-09-22 20:43:47 -0300

    concertando o Github Pages

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let table = function(x,y){
+let tableMaker = function(x,y){
     let result = []
     for(let i = 0; i <= x; i++){
         result.push([])
         for (let j = 0; j <= y; j++){
             result[i].push(i*j)
         }
-    }return console.table(result) 
+    }return console.table(result)
 }
 
-table(10,10)
+tableMaker(10,10)
 
-console.log('digite table(valorX da table,valorY da table) no console para ver a tabela com o tamanho desejado aparecer ;)')
-console.log('ex: table(12,12)')
+console.log('digite tableMaker(valorX da table,valorY da table) no console para ver a tabela com o tamanho desejado aparecer ;)')
+console.log('ex: tableMaker(12,12)')
